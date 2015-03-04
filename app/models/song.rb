5b79bb5ea266c2004a3256b6f5ea5a35c1a95aec
James Dabbs <jamesdabbs@gmail.com>
2015-03-04 10:17:37 -0500

    Add a validation spec

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :user
 
+  validates_presence_of :title, :artist
+
   def self.unvetoed
     where vetoed: false
   end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Song do
   it "allows users to veto songs" do
     user = User.create! email: "a@example.com", password: "password"
-    song = Song.create!
+    song = Song.create! title: "A Thing", artist: "The People"
 
     expect(Song.unvetoed).to include song
 
@@ -11,5 +11,9 @@ describe Song do
     expect(Song.unvetoed).not_to include song
   end
 
-  it "validates that songs have titles"
+  it "validates that songs have titles" do
+    expect do
+      Song.create!
+    end.to raise_error ActiveRecord::RecordInvalid
+  end
 end
