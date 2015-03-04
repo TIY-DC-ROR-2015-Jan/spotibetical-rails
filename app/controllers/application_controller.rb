51f1fd925bd3ac6007f4bacf7bcf97487e56189e
James Dabbs <jamesdabbs@gmail.com>
2015-03-04 11:59:32 -0500

    Login-protect routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def home
-    redirect_to songs_path
-  end
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # post '/songs/:id/veto' => 'songs#veto'
 
-  root 'application#home'
+  root 'songs#index'
 end
